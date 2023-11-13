3f6d30b2063d2aa3d555c6d06b449a743d9b4079
jangwone <wone45@hanmail.net>
2023-11-13 23:45:25 +0900

    message properties

--- src/main/java/com/everyday/rest/webservices/restfulwebservices/work/WorkController.java
+++ src/main/java/com/everyday/rest/webservices/restfulwebservices/work/WorkController.java
@@ -1,5 +1,9 @@
 package com.everyday.rest.webservices.restfulwebservices.work;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 //REST API
 @RestController
 public class WorkController {
+	
+	private MessageSource messageSource;
+	
+	public WorkController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping("test")
 	public String abc() {
@@ -24,5 +34,12 @@ public class WorkController {
 	public JsonBean beanNamePathVariable(@PathVariable String name) {
 		return new JsonBean(
 				String.format("hello , %s", name));
+	}
+	
+	@GetMapping("test-message")
+	public String abcInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		String message = messageSource.getMessage("hello.message", null, "Defalt Message", locale);
+		return message;
 	}
 }
