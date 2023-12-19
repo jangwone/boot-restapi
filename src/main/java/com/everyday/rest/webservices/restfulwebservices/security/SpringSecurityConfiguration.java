8e84592e2f3ed4a72fece8ae7708e5bf8662783f
jangwone <wone45@hanmail.net>
2023-12-19 23:32:30 +0900

    jwt

--- src/main/java/com/everyday/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
+++ src/main/java/com/everyday/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class SpringSecurityConfiguration {
 	
 	@Bean 
